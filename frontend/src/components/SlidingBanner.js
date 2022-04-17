6c497eaa47c1bffea324cf64df0e80767170245e
Justin Kim <justin.kim3420@gamil.com>
2022-04-17 19:25:54 -0400

    created sliding banner

--- frontend/src/components/SlidingBanner.js
+++ frontend/src/components/SlidingBanner.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SlidingBanner = () => {
-  return <div className="sliding-banner">
-      hello
-  </div>;
+  const [currentPage, setPage] = useState(0);
+
+  let testEvents = [
+    <div style={{ "backgroundColor": "green" }} key='1' className="event">
+      event1
+    </div>,
+    <div style={{ "backgroundColor": "yellow" }} key='2' className="event">
+      event2
+    </div>,
+    <div style={{ "backgroundColor": "white" }} key='3' className="event">
+      event3
+    </div>,
+    <div style={{ "backgroundColor": "blue" }} key='4' className="event">
+      event4
+    </div>,
+  ];
+
+  const handleClick = (direction) => {
+    console.log(direction,currentPage)
+    if (direction === "LEFT") {
+      if (currentPage === 0) {
+        setPage(testEvents.length - 1);
+      } else {
+        setPage(currentPage - 1);
+      }
+    }
+    if (direction === "RIGHT") {
+      if (currentPage === testEvents.length - 1) {
+        setPage(0);
+      } else {
+        setPage(currentPage + 1);
+      }
+    }
+  };
+
+  return (
+    <div className="sliding-banner">
+      <div className="slideshow">
+        <div
+          className="left-arrow"
+          onClick={() => {
+            handleClick("LEFT");
+          }}
+        ></div>
+        <div className="slides" style={{'left':(`${-100*currentPage}%`)}}>{testEvents}</div>
+        <div
+          className="right-arrow"
+          onClick={() => {
+            handleClick("RIGHT");
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default SlidingBanner;
